e3b35134e24ec4d90aba9b6230bebb420eed1c42
Andr <andre199773@mail.ru>
2022-03-30 10:38:28 +0100

    ask user about films

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-let numberOfFilms = 0;
-alert("Сколько фильмов вы уже посмотрели", numberOfFilms );
-console.log(numberOfFilms);
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "0" );
+
+
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+let a = prompt("LAst film your watch?", "nan");
+let b = prompt("your grade this film", "8.1");
+let c = prompt("LAst film your watch?", "nan");
+let d = prompt("your grade this film", "8.1");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB); 
