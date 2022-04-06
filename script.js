a3029d93c59bb61eed269eac1420fcfaac472783
Andr <andre199773@mail.ru>
2022-04-06 20:16:32 +0100

    Add ask about film

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "0" );
 
 
@@ -11,12 +13,75 @@ let personalMovieDB = {
 };
 
 
-let a = prompt("LAst film your watch?", "nan");
-let b = prompt("your grade this film", "8.1");
-let c = prompt("LAst film your watch?", "nan");
-let d = prompt("your grade this film", "8.1");
+for (let i = 0; i < numberOfFilms; i++){
+    let a = prompt("Last film your watch?", "nan");
+    let b = prompt("your grade this film", "8.1");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a.length < 50 && b.length < 50 && a.length != 0 && b.length != 0){
+        console.log(a);
+        console.log('ok');
+    }else{
+        console.log('error');
+        i--;
+    }
 
-console.log(personalMovieDB); 
+    personalMovieDB.movies[a] = b;
+}
+ if(personalMovieDB.count < 10 ){
+    console.log("Посмотрено довольно мало фильмов");
+ }  
+ else if(personalMovieDB.count > 10 && personalMovieDB.count < 30 ){
+    console.log("Вы класический зритель");
+ }
+ else if(personalMovieDB.count > 30 ){
+    console.log("Вы киноман");
+ } else{
+    console.log("Erorrr");
+ }
+
+
+
+console.log(personalMovieDB);
+/*
+if (4 == 4) {
+    console.log('Ok!');
+}else{
+    console.log('error');
+}
+
+const nim = 50;
+
+if (nim < 49){
+    console.log('March');
+
+}else if (nim > 100){
+     
+}
+(num === 50) ? console.log('Ok!') : console.log("error");
+
+switch (num) {
+    case 49:
+        console.log('not correct');
+        break;
+    case 50:
+        console.log('Ok');
+        break;
+    default:
+        console.log('not now(');
+}
+while (num < 55) {
+    console.log(num);
+    num++;
+
+}
+
+
+let num = 50;
+
+
+
+for (let i = 1; i < 8; i++){
+    console.log(num);
+    num++;
+}
+*/
